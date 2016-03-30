d69ee892b38081b8a1dd44e412ad3754bae20f95
ca2 <camilo@ca2.email>
2016-03-30 03:56:38 +0000

    Mummi contribution!! veriverse contribution!! LCTV contribution!! Lnu.se contribution (SWE)!! catherinacortega contribution!! cppjiveturkey contribution!! ggc (Davit from Armenia) contribution!! ixayldz (isa yildiz) collaboration!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    from bilbo at veriverse
    i7 6700

---
+++ latin_america/brazil/camilo/config/irc_resident/room/camilosasuke@irc.twitch.tv/irc_bot.cpp
@@ -0,0 +1,32 @@
+#include "framework.h"
+#include "app-core/appseed/irc_resident/irc_resident.h"
+#pragma comment (lib,"vericard.lib" )
+#pragma comment (lib,"irc.lib" )
+#pragma comment (lib,"app_core_irc_bot.lib" )
+#pragma comment (lib,"app_core_irc_client.lib" )
+#pragma comment (lib,"app_core_irc_resident.lib" )
+
+
+class this_plugin :
+	virtual public ::irc_resident::plugin
+{
+	
+	public:
+	
+	
+      this_plugin(::aura::application * papp, object * p) :
+		object(papp),
+      ::vericard::plugin(papp,dynamic_cast <::vericard::comm * > (p)),
+		::irc_resident::plugin(papp, dynamic_cast <::vericard::comm * > (p))
+
+#include "C:\ca2\config\xmpp_resident\room\ca2@livecoding.tv\xmpp_bot_common.cpp"
+
+};
+
+
+BEG_ONLY_FACT
+CREATE_OBJECT_ENTRY("plugin",this_plugin)
+END_ONLY_FACT
+
+
+
