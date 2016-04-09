5a3eb8c9ceb8d8bd40deebd6c84d1371063e03dc
qamilox@uol.com.br <qamilox@uol.com.br@c5eec057-a948-424f-8865-a8778a9986e5>
2016-04-09 15:11:38 +0000

    Mummi contribution!! veriverse contribution!! LCTV contribution!! Lnu.se contribution (SWE)!! catherinacortega contribution!! cppjiveturkey contribution!! ggc (Davit from Armenia) contribution!! ixayldz (isa yildiz) collaboration!! fuel cowork!! thomasbs streamer contribution!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    from bilbo at veriverse
    i7 6700

--- user/latin_america/brazil/camilo/config/xmpp_resident/room/ca2@livecoding.tv/xmpp_bot.cpp
+++ user/latin_america/brazil/camilo/config/xmpp_resident/room/ca2@livecoding.tv/xmpp_bot.cpp
@@ -10,8 +10,8 @@ class this_plugin:
    virtual public ::xmpp_resident::plugin
 {
 public:
-   
-   
+      
+
    this_plugin(::aura::application * papp, object * p) :
       object(papp),
       ::vericard::plugin(papp, dynamic_cast <::vericard::comm *> (p)),
