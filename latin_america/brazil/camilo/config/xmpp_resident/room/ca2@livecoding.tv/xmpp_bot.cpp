b241bf719f9f35b0dbb4ad2a1eca668a2130e1dc
ca2 <camilo@ca2.email>
2016-04-18 13:09:14 +0000

    from bilbo machine at veriverse!!

--- latin_america/brazil/camilo/config/xmpp_resident/room/ca2@livecoding.tv/xmpp_bot.cpp
+++ latin_america/brazil/camilo/config/xmpp_resident/room/ca2@livecoding.tv/xmpp_bot.cpp
@@ -9,7 +9,10 @@
 class this_plugin:
    virtual public ::xmpp_resident::plugin
 {
-public:   
+public:  
+   
+
+   
    this_plugin(::aura::application * papp, object * p) :
       object(papp),
       ::vericard::plugin(papp, dynamic_cast <::vericard::comm *> (p)),
