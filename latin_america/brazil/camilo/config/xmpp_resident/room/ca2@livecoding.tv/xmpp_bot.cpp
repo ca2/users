b5ccd4b1d7986a822e8ec3eb79a5cc40c27705aa
ca2 <camilo@ca2.email>
2016-05-09 21:13:01 +0000

    veriverse contribution!! Mummi and bilbo contribution!! LCTV app-ios competition contribution!! catherinacortega, fuel, phanxgames contribution!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    bilbo at veriverse!!

--- latin_america/brazil/camilo/config/xmpp_resident/room/ca2@livecoding.tv/xmpp_bot.cpp
+++ latin_america/brazil/camilo/config/xmpp_resident/room/ca2@livecoding.tv/xmpp_bot.cpp
@@ -11,15 +11,12 @@
 #define __XMPP
 
 
-
-
 class this_plugin:
    virtual public ::xmpp_resident::plugin
 {
 public:  
-   
-      
-   
+
+
    this_plugin(::aura::application * papp, object * p) :
       object(papp),
       ::vericard::plugin(papp, dynamic_cast <::vericard::comm *> (p)),
@@ -32,6 +29,14 @@ public:
    BEG_ONLY_FACT
    CREATE_OBJECT_ENTRY("plugin",this_plugin)
    END_ONLY_FACT
+
+
+
+
+
+
+
+
 
 
 
