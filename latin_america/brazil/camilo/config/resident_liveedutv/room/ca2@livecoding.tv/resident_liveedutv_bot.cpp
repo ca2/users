5aee941b35fea15bfb087035fa4098ccaeb46f19
ca2 <camilo@ca2.email>
2017-01-20 12:11:28 +0000

    YEY! From bilbo-macos!! bilbo and Mummi contribution!! veriverse contribution!! LCTV contribution!! catherinacortega (Catherina Cortega from Nederlands, Zwijndrecht) and fuel contribution!! ade, intelijens, kaeris and trump contribution!! hosjenken, sonnystarminecraft (Vlad-Andrei Ionescu from Bucharest, Romania) and vapaamies contribution!! theaifam5 contribution!! dopedopedope and johnyblaze contribution!! Twitch.tv contribution!! Tectuma (and mates contribution..) contribution...!! carolkiyoko contribution!! Carol and Lucinho contribution!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    from bilbo veriverse OS X!!
    i7 6700 32GB RAM 120 GB SSD 240 GB SSD 2TB HDD

---
+++ latin_america/brazil/camilo/config/resident_liveedutv/room/ca2@livecoding.tv/resident_liveedutv_bot.cpp
@@ -0,0 +1,144 @@
+#include "resident.h"
+#pragma comment (lib,"announcecom.lib" )
+#pragma comment (lib,"vericard.lib" )
+#pragma comment (lib,"app_core_resident.lib" )
+#pragma comment (lib,"user32.lib" )
+
+
+#include "app-core/appseed/vericard/vericard_animation.h"
+
+#include "vericard_animation_disco.cpp"
+
+
+#define __XMPP
+
+
+
+
+class this_plugin:
+   virtual public ::vericard::plugin
+{
+public:  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+  
+
+
+      
+   this_plugin(::aura::application * papp, object * p) :
+      object(papp),
+      ::vericard::chat_item(papp),
+      ::vericard::plugin(papp, dynamic_cast <::vericard::comm *> (p))
+
+#include "bot_common.cpp"
+
+
+
+
+      ::vericard::animation * create_animation(string strAnimation)
+   {
+
+      if (strAnimation == "disco")
+      {
+
+         return new ::vericard::animation_disco(&Application.veripack());
+
+      }
+
+      return NULL;
+
+   }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+   BEG_ONLY_FACT
+   CREATE_OBJECT_ENTRY("plugin",this_plugin)
+   END_ONLY_FACT
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
---
+++ latin_america/brazil/camilo/config/resident_twitchtv/room/ca2live@irc.twitch.tv/resident_twitchtv_bot.cpp
@@ -0,0 +1,45 @@
+﻿#include "framework.h"
+#pragma comment (lib,"announcecom.lib" )
+#pragma comment (lib,"vericard.lib" )
+#pragma comment (lib,"app_core_resident.lib" )
+#pragma comment (lib,"user32.lib" )
+
+#define __IRC
+
+
+class this_plugin :
+	virtual public ::vericard::plugin
+{
+
+
+	public:
+	
+	
+
+
+
+	
+
+
+
+
+      
+
+      this_plugin(::aura::application * papp, object * p) :
+		object(papp),
+      ::vericard::chat_item(papp),
+      ::vericard::plugin(papp,dynamic_cast <::vericard::comm * > (p))
+
+
+		#include "../../../resident_liveedutv/room/ca2@livecoding.tv/bot_common.cpp"
+
+
+};
+
+
+BEG_ONLY_FACT
+CREATE_OBJECT_ENTRY("plugin",this_plugin)
+END_ONLY_FACT
+
+
+
